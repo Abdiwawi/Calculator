497b15173ebd4b16332cf89698606b09eb047d73
Abdiwawi <abdijamzilan@gmail.com>
2017-04-20 18:48:31 +0300

    add new files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var add = function(number1, number2) {
+      return number1 + number2;
+    };
+
+    var subtract = function(number1, number2) {
+      return number1 - number2;
+    };
+
+    var multiply = function(number1, number2) {
+      return number1 * number2;
+    };
+
+    var divide = function(number1, number2) {
+      return number1 / number2;
+    };
+    $(document).ready(function() {
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+
+      alert(add(number1, number2));
+    });
+    $("#add1").val();
+    $("add2").val();
+    $(document).ready(function() {
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        alert(add(number1, number2));
+      });
